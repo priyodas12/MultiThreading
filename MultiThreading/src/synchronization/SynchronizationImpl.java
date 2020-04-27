4453039b39b1317df5c652e2b9c724ef94c2459f
Priyobrato Das <priyodas12@gmail.com>
2020-04-27 23:52:25 +0530

    synchonization added

--- MultiThreading/src/synchronization/SynchronizationImpl.java
+++ MultiThreading/src/synchronization/SynchronizationImpl.java
@@ -1,5 +1,12 @@
 package synchronization;
 
+/*
+ * modifier applicable for blocks and methods
+ * variable,class cannot be declared.
+ * 
+ * If multiple thread trying to access same object and modify state then there will be data inconsistency(Race Condition)
+ * problem to avoid at a time one thread can be allowed by using synchronized modifier in method or blocks
+ * */
 public class SynchronizationImpl {
 
 	public static void main(String[] args) {
