748d85cd8209042cabd3fe863de3a528fb9e4672
Priyobrato Das <priyodas12@gmail.com>
2020-04-28 00:20:35 +0530

    synchronization

--- MultiThreading/src/synchronization/SynchronizationImpl.java
+++ MultiThreading/src/synchronization/SynchronizationImpl.java
@@ -1,5 +1,7 @@
 package synchronization;
 
+import java.util.Date;
+
 /*
  * modifier applicable for blocks and methods
  * variable,class cannot be declared.
@@ -8,10 +10,51 @@ package synchronization;
  * problem to avoid at a time one thread can be allowed by using synchronized modifier in method or blocks
  * */
 public class SynchronizationImpl {
-
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		TimeZone t=new TimeZone();
+		Thread t1=new Thread(new TestSync(t,"Bangalore"));
+		Thread t2=new Thread(new TestSync(t,"Chennai"));
+		t1.start();
+		t2.start();
 	}
 
 }
+
+class TestSync implements Runnable{
+	
+	TimeZone t1;
+	String place;
+	
+	
+	public TestSync(TimeZone t1, String place) {
+		super();
+		this.t1 = t1;
+		this.place = place;
+	}
+
+	@Override
+	public void run() {
+		System.out.println("modifying by "+Thread.currentThread().getName());
+		try {
+			t1.getDateTimeInfo(place);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}	
+}
+
+class TimeZone {
+	
+	private String place;
+	
+	public  void getDateTimeInfo(String place) throws InterruptedException {
+		for(int i=0;i<10;i++) {
+			System.out.println(i+">> Place: "+place);
+			Thread.sleep(2000);
+			System.out.println("Time: "+new Date());
+		}
+	}
+}
+
